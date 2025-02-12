92d39bcd29bae140748488f880a6643d0f23e9f9
rahyanakil <rahyanakil89@gmail.com>
2025-02-12 14:00:46 +0600

    error solved

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import *as taskController from"../controllers/taskController.js"
-import { PORT } from "../config/config";
+import { PORT } from "../config/config.js";
 const router =express.Router();
 
 
