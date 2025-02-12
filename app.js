04561fca7327ea54511d3da540c2a2fe4b1a0fa0
rahyanakil <rahyanakil89@gmail.com>
2025-02-12 16:33:15 +0600

    request limit setup done

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import mongoose from "mongoose";
 import router from "./routes/api.js"
-import { PORT } from "./config/config.js";
+import { PORT,REQUEST_LIMIT_NUMBER,REQUEST_LIMIT_TIME} from "./config/config.js";
 
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
 
 const app = express();
+const limiter =rateLimit({
+    windowMs:REQUEST_LIMIT_TIME,
+    max:REQUEST_LIMIT_NUMBER,
+})
+
+app.use(limiter);
 app.use("/api",router);
 
 app.listen(PORT,()=>{
--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 export const PORT =5050;
+export const REQUEST_LIMIT_TIME= 15*60*1000//15minutes
+export const REQUEST_LIMIT_NUMBER=2000
