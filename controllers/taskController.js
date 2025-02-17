164be250dc0ad8915931bd983bdac0432a6d1852
rahyanakil <rahyanakil89@gmail.com>
2025-02-17 17:04:16 +0600

    query string setup done with an error

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,11 +1,16 @@
 //url params(url parameter)
 export const demo1 =async(req,res)=>{
-
+let name =req.params.name;
+let age =req.params.age;
+return res.json({name:name,age:age});
 }
 
 //query string
 export const demo2 = async(req,res)=>{
 
+let name =req.query.name;
+let age = req.query.age;
+return res.json({name:name,age:age});
 }
 
 //how to work with request body json
