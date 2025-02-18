81f7e9a2f87d9bccb79901f33fac3e42136d7c60
rahyanakil <rahyanakil89@gmail.com>
2025-02-18 14:15:43 +0600

    demo3 and demo4 works with error and simple middle wire setup done

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -6,6 +6,7 @@ return res.json({name:name,age:age});
 }
 
 //query string
+// http://localhost:5050/api/demo2?name=Rahyan&age=34
 export const demo2 = async(req,res)=>{
 
 let name =req.query.name;
@@ -15,15 +16,24 @@ return res.json({name:name,age:age});
 
 //how to work with request body json
 export const demo3 = async(req,res)=>{
-
+let JSONBody = req.body
+return res.json(JSONBody);
 }
 
 //how to work with request form data file
 export const demo4 = async(req,res)=>{
+    let FormDataBody =req.body
+    return res.json(FormDataBody)
 
 }
 
 //request header properties
 export const demo5 = async(req,res)=>{
+ let reqHeader =req.headers;
+ return res.json(reqHeader)
+}
 
+//how to work with middle wire
+export const demo6 =async(req,res)=>{
+    return res.json()
 }
---
+++ middlewares/authmiddleware.js
@@ -0,0 +1,5 @@
+module.exports =(req,res,next)=>{
+    //code
+    console.log("Iam from middle wires")
+    next();
+}
